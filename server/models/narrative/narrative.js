b44d5ec85498be3febba81b3626e779da77bc349
Kirsten Lindblom <kirsten.m.lindblom@gmail.com>
2019-11-21 21:19:32 -0800

    Displaying information in modal in narrative tab

--- server/models/narrative/narrative.js
+++ server/models/narrative/narrative.js
@@ -2,31 +2,31 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ESOadditions', { useNewUrlParser: true });
 
 const NarrativeSchema = new mongoose.Schema({
-    impression: [
-        { primaryImpression: { type: String } },
-        { secondaryImpression: { type: String } },
-        { medicalTrauma: { type: String } }
-    ],
-    supportingSignsSymptoms: [{
-        category: { type: String }
-    }],
-    patientComplaint: [
-        { chiefComplaint: { type: String } },
-        { secondaryComplaint: { type: String } },
-        { finalPatientAcuity: { type: String } }
-    ],
-    injuries: [
-        { wasInjured: { type: Boolean } },
-        { primaryInjury: { type: String } },
-        { injuryDetails: { type: String } },
-        { placeOfInjury: { type: String } },
-        { dateOfInjury: { type: Date } }
-    ],
-    factorsAffectingCare: [
-        { barriersToCare: { type: String } },
-        { alcoholDrugs: { type: String } },
-        { isPregnant: { type: Boolean } }
-    ],
+    // impression: [
+    //     { primaryImpression: { type: String } },
+    //     { secondaryImpression: { type: String } },
+    //     { medicalTrauma: { type: String } }
+    // ],
+    // supportingSignsSymptoms: [{
+    //     category: { type: String }
+    // }],
+    // patientComplaint: [
+    //     { chiefComplaint: { type: String } },
+    //     { secondaryComplaint: { type: String } },
+    //     { finalPatientAcuity: { type: String } }
+    // ],
+    // injuries: [
+    //     { wasInjured: { type: Boolean } },
+    //     { primaryInjury: { type: String } },
+    //     { injuryDetails: { type: String } },
+    //     { placeOfInjury: { type: String } },
+    //     { dateOfInjury: { type: Date } }
+    // ],
+    // factorsAffectingCare: [
+    //     { barriersToCare: { type: String } },
+    //     { alcoholDrugs: { type: String } },
+    //     { isPregnant: { type: Boolean } }
+    // ],
     narrative: { type: String }
 }, { timestamps: true })
 
